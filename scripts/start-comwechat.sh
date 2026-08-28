fbfe6481b165fe1905ef948e708832a714bb9a42
jiz4oh <git@jiz4oh.com>
2026-08-28 17:49:45 +0800

    chore: remove obsolete Docker assets and script

--- scripts/start-comwechat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-wine_prefix="${WINEPREFIX:-/home/user/.wine}"
-if [ -d "$wine_prefix" ]; then
-    chown -R user:group "$wine_prefix"
-fi
-
-exec /bin/su -s /bin/sh user -c 'exec /bin/dumb-init "$@"' /bin/sh "$@"
